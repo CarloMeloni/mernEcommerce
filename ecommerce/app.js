3d5210f9dcb27f040905afd3aa506b57bd9a7cf1
carloMeloni <carlo.meloni@hotmail.it>
2020-09-09 11:43:54 +0200

    User Signup

--- ecommerce/app.js
+++ ecommerce/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 //ROUTES
 const userRoutes = require('./routes/user');
@@ -12,6 +15,11 @@ mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useCreateIndex: true
 }).then(() => { console.log('DATABASE CONNECTED!') });
+
+//MIDDLEWARES
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 //ROUTES MIDDLEWARE
 app.use('/api', userRoutes);
--- ecommerce/controllers/user.js
+++ ecommerce/controllers/user.js
@@ -1,5 +1,16 @@
-exports.sayHello = (req, res) => {
-    res.json({
-        message: "Hey guys how are you"
-    })
+const User = require('../models/user');
+
+exports.signup = (req, res) => {
+    const user = new User(req.body);
+    console.log("req.body:", req.body);
+    user.save((err, user) => {
+        if(err) {
+            return res.status(400).json({
+                error
+            });
+        }
+        res.json({
+            user
+        });
+    });
 };
